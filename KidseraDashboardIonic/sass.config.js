2adf32704d0aaf6073b8e67ce3618a7acb1a4f45
UnFik <89504918+UnFik@users.noreply.github.com>
2022-11-25 13:34:55 +0700

    Revert "Menambahkan folder ionic"

--- KidseraDashboardIonic/sass.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  includePaths: [
-    "node_modules/ionic-vue/themes",
-    "node_modules/ionicons/scss",
-    "node_modules/ionic-vue/fonts",
-    "node_modules/bootstrap/scss",
-  ],
-};
